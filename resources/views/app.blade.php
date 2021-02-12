8af39d7b00fca94865ea26c14087b48c088b0128
heinkel <juan.ortiz.tandil@gmail.com>
2021-02-12 09:24:59 +0900

    almost set inertia for VUEJS

---
+++ resources/views/app.blade.php
@@ -0,0 +1,25 @@
+{{-- <!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Vue Inertia App2</title>
+
+    </head>
+    <body class="antialiased">
+            <p style="text-align:center;">Hi there!</p>
+    </body>
+</html> --}}
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
+    <script src="{{ mix('/js/app.js') }}" defer></script>
+  </head>
+  <body>
+    @inertia
+  </body>
+</html>
